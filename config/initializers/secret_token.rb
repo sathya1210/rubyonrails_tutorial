a78e7d0b242c3f54129a87d61ffda9c871c39536
sathya nammalwar <sathya.nammalwar@gmail.com>
2014-08-14 23:13:01 +1000

    initial commit done

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myappsai::Application.config.secret_token = '4c719115eed10e9592557dd782e935d7824d28790e968ae16800ed5eb4784413e4967a175737f905f6c34006a96f374cf14b18543881606b8b41faab501648a9'
